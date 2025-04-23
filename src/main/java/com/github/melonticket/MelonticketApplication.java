b607156754e7c83b5561a8da2786ebc18e135859
Your Name <you@example.com>
2025-04-23 13:31:57 +0800

    init project

---
+++ src/main/java/com/github/melonticket/MelonticketApplication.java
@@ -0,0 +1,13 @@
+package com.github.melonticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelonticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelonticketApplication.class, args);
+	}
+
+}
