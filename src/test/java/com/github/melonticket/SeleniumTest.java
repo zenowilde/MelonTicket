74ea4c113eda318c28533769a3dd8f635e1b0708
Your Name <you@example.com>
2025-04-24 12:29:52 +0800

    feat: 添加Selenium依赖并创建基础测试类

---
+++ src/test/java/com/github/melonticket/SeleniumTest.java
@@ -0,0 +1,35 @@
+package com.github.melonticket;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class SeleniumTest {
+    public static void main(String[] args) {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--window-size=1920,1080");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://read.douban.com/reader/column/4639817/chapter/24562734/?dcs=column&dcm=chapter-list");
+
+        // 显式等待标题元素加载
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement titleContentElement = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='chapter-header']/h1"))
+        );
+        String titleContent = titleContentElement.getText();
+        System.out.println(titleContent);
+
+        //获取文章内容（此div包含标题）
+        WebElement articleContentElement = driver.findElement(By.xpath("//div[@class='article']"));
+        String articleContent = articleContentElement.getText();
+        System.out.println(articleContent);
+    }
+}
