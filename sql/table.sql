3b7c0cbde3b7ea99640814bb16bacecf13246031
Your Name <you@example.com>
2025-06-14 14:27:34 +0800

    feat(crawler): 更新数据库表结构并添加 Chrome WebDriver 配置

--- sql/table.sql
+++ sql/table.sql
@@ -1,12 +1,15 @@
-CREATE TABLE concert (
-    id VARCHAR(255) PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    type VARCHAR(255),
-    imgUrl VARCHAR(255),
-    date VARCHAR(255),
-    venue VARCHAR(255),
-    genre VARCHAR(255),
-    rating VARCHAR(255),
-    createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+-- melonticket.concert definition
+
+CREATE TABLE `concert` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `img_url` varchar(255) DEFAULT NULL,
+  `date` varchar(255) DEFAULT NULL,
+  `venue` varchar(255) DEFAULT NULL,
+  `genre` varchar(255) DEFAULT NULL,
+  `rating` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=270 DEFAULT CHARSET=utf8mb4;
